a39662ab2de118656c799f5142414538afbcb61f
suchandra-rltest <suchandra-rltest@gmail.com>
2018-02-10 17:29:08 +0530

    added latest files

--- app/src/main/java/com/radiolocus/beaconproberifaceexample/MainActivity.java
+++ app/src/main/java/com/radiolocus/beaconproberifaceexample/MainActivity.java
@@ -5,30 +5,41 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 
-import com.radiolocus.beaconproberiface.interfaces.RadioLocusBeaconScannerCallback;
+import com.radiolocus.beaconproberiface.interfaces.IRadioLocusBeaconScannerCallback;
 import com.radiolocus.beaconproberiface.utility.RlAltBeacon;
 import com.radiolocus.beaconproberiface.utility.RadioLocusBeaconScanner;
 
 public class MainActivity extends AppCompatActivity
-            implements RadioLocusBeaconScannerCallback  {
+            implements IRadioLocusBeaconScannerCallback  {
     BluetoothManager mBluetoothManager = null;
-
+    RadioLocusBeaconScanner bpm = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
+        mBluetoothManager = (BluetoothManager) getApplicationContext().getSystemService(getApplicationContext().BLUETOOTH_SERVICE);
+        bpm = new RadioLocusBeaconScanner(getApplicationContext(),this,mBluetoothManager);
+        startRadioLocusBeaconScan();
     }
 
 
     @Override
     public void startRadioLocusBeaconScan() {
 
-    }
+        bpm.StartScanning();
+      }
 
     @Override
     public void stopRadioLocusBeaconScan() {
+       // RadioLocusBeaconScanner bpm = new RadioLocusBeaconScanner(getApplicationContext(),this,mBluetoothManager);
+        bpm.Stop();
+
+    }
+
+    @Override
+    public void restartRadioLocusBeaconScan() {
+        //RadioLocusBeaconScanner bpm = new RadioLocusBeaconScanner(getApplicationContext(),this,mBluetoothManager);
+        bpm.reStartScanning();
 
     }
 }
